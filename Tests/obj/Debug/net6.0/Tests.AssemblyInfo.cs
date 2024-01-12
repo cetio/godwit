6d2cde636c14f0bf5cf369356ee0bf6ab3fcf278
cet <stakeandtake39@gmail.com>
2024-01-12 12:37:09 -0500

    * Fixed array support!

--- Tests/obj/Debug/net6.0/Tests.AssemblyInfo.cs
+++ Tests/obj/Debug/net6.0/Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b203583eec1e76a3bebe90c8dfddb8ef2e4cef83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3f4088e1a42d7ab25a8f7d0df7cdb715b64618c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
